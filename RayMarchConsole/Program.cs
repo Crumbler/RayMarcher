a3a247efbf3edf13fde7bc43e2ef82c271ad24e3
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-06-30 17:01:55 +0300

    Create RayMarchLib and RayMarchConsole

---
+++ RayMarchConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace RayMarchConsole
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
