5ffa38ec2fd9e52da4f4dd12c1bf10d3db3c4548
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-07-24 14:26:36 +0300

    Add Box object

---
+++ RayMarchLib/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace RayMarchLib
+{
+    public class Box : RMObject
+    {
+        public Vector3 Size { get; set; }
+
+        public Box()
+        {
+            Size = new Vector3(0.5f);
+        }
+
+        protected override float GetDist(Vector3 v)
+        {
+            var q = Vector3.Abs(v) - Size;
+            return Vector3.Max(q, Vector3.Zero).Length() + 
+                MathF.Min(MathF.Max(q.X, Math.Max(q.Y, q.Z)), 0.0f);
+        }
+    }
+}
