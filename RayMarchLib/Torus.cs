ed1787bd92157bf027c37116674f23f20e657e2f
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-07-27 13:18:22 +0300

    Add Torus

---
+++ RayMarchLib/Torus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace RayMarchLib
+{
+    public class Torus : RMObject
+    {
+        public float InnerRadius { get; set; }
+        public float OuterRadius { get; set; }
+
+        public Torus()
+        {
+            InnerRadius = 0.5f;
+            OuterRadius = 0.1f;
+        }
+
+        protected override float GetDist(Vector3 v)
+        {
+            var q = new Vector2(new Vector2(v.X, v.Z).Length() - InnerRadius, v.Y);
+
+            return q.Length() - OuterRadius;
+        }
+    }
+}
