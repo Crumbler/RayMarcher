da66704ec48d0ec5ac1a3b2841f8f97fc66326c9
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-07-27 13:29:31 +0300

    Add Clamp method

--- RayMarchLib/Utils.cs
+++ RayMarchLib/Utils.cs
@@ -4,6 +4,19 @@ namespace RayMarchLib
 {
     public static class Utils
     {
+        public static float Clamp(float x, float a, float b)
+        {
+            if (x < a)
+            {
+                return a;
+            }
 
+            if (x > b)
+            {
+                return b;
+            }
+
+            return x;
+        }
     }
 }
