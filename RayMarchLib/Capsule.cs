55b946caed9da22cf9168d6a90f4f1e630aaa8af
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-07-27 13:29:37 +0300

    Add Capsule

---
+++ RayMarchLib/Capsule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace RayMarchLib
+{
+    public class Capsule : RMObject
+    {
+        public float Length { get; set; }
+        public float Radius { get; set; }
+
+        public Capsule()
+        {
+            Length = 1.0f;
+            Radius = 0.1f;
+        }
+
+        protected override float GetDist(Vector3 v)
+        {
+            v.Y -= Utils.Clamp(v.Y, -Length / 2.0f, Length / 2.0f);
+
+            return v.Length() - Radius;
+        }
+    }
+}
